45fb4c8839c79b7b5d62b5bad5a95c0219c7fd52
jonathanburrows <jonathanburrows@live.com>
2016-12-23 22:34:26 -0800

    Moving fixtures into folders, setting up Web and Web.Tests

---
+++ lvl.Web.Tests/Fixtures/WebServiceProviderFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace lvl.Web.Tests.Fixtures
+{
+    public class WebServiceProviderFixture
+    {
+        public IServiceProvider ServiceProvider { get; }
+
+        public WebServiceProviderFixture()
+        {
+            ServiceProvider = new ServiceCollection()
+                .AddDomains()
+                .AddRepositories()
+                .AddWeb()
+                .BuildServiceProvider();
+        }
+    }
+}
---
+++ lvl.Web.Tests/WebServicesTests.cs
@@ -0,0 +1,26 @@
+﻿using lvl.Web.Tests.Fixtures;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace lvl.Web.Tests
+{
+    public class WebServicesTests : IClassFixture<WebServiceProviderFixture>
+    {
+        private IServiceProvider Services { get; }
+
+        public WebServicesTests(WebServiceProviderFixture webServiceProviderFixture)
+        {
+            Services = webServiceProviderFixture.ServiceProvider;
+        }
+
+        [Fact]
+        public void WhenAddingWeb_CallsAddMvc()
+        {
+            var controllerFactory = Services.GetRequiredService<IControllerFactory>();
+
+            Assert.NotNull(controllerFactory);
+        }
+    }
+}
