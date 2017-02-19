9bf9b4f1df6ca4e067b03cbe77ec74545727002a
jonathanburrows <jonathanburrows@live.com>
2017-02-18 22:09:11 -0800

    adding min-length validation decorator

---
+++ @lvl.core/src/decorators/min-length.decorator.spec.ts
@@ -0,0 +1,48 @@
+﻿import { MinLength } from './min-length.decorator';
+import { GetValidationRules } from './validation-factory';
+
+describe(MinLength.name, () => {
+    it('is valid if value is null', () => {
+        const person = new Person();
+        const rules = GetValidationRules(person);
+
+        const isValid = rules.every(rule => rule(person));
+
+        expect(isValid).toBe(true);
+    });
+
+    it('is invalid if value is less than MinLength', () => {
+        const person = new Person();
+        person.fullName = '12';
+        const rules = GetValidationRules(person);
+
+        const isValid = rules.every(rule => rule(person));
+
+        expect(isValid).toBe(false);
+    });
+
+    it('is valid if value is equal to MinLength', () => {
+        const person = new Person();
+        person.fullName = '123';
+        const rules = GetValidationRules(person);
+
+        const isValid = rules.every(rule => rule(person));
+
+        expect(isValid).toBe(true);
+    });
+
+    it('is valid if value is larger than MinLength', () => {
+        const person = new Person();
+        person.fullName = '1234';
+        const rules = GetValidationRules(person);
+
+        const isValid = rules.every(rule => rule(person));
+
+        expect(isValid).toBe(true);
+    });
+});
+
+class Person {
+    @MinLength(3)
+    fullName: string;
+}
---
+++ @lvl.core/src/decorators/min-length.decorator.ts
@@ -0,0 +1,27 @@
+﻿import { DefineValidationMetadata } from './validation-factory';
+
+/**
+ * Specifies the minimum length of array or string data allowed in a property.
+ */
+export function MinLength(length: number): PropertyDecorator {
+    return (target: Object, propertyKey: string) => {
+        if (!length) {
+            throw new Error(`@${MinLength.name} on ${target.constructor.name} has an undefined length.`);
+        }
+        if (length < 0) {
+            throw new Error(`@${MinLength.name} on ${target.constructor.name} has a negative number.`);
+        }
+
+        const isValid = (validating) => {
+            const value = validating[propertyKey];
+
+            if (!value) {
+                return true;
+            }
+
+            return value.toString().length >= length;
+        };
+
+        DefineValidationMetadata(MinLength.name, isValid, target, propertyKey);
+    };
+}
