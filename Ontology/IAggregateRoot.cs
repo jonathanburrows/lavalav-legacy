cbc08c85d276e99f34cad6ed9035ed156a803a7f
jonathanburrows <jonathanburrows@live.com>
2017-05-07 15:36:01 -0700

    added the IAggregateRoot and IAggregateScope interfaces, in accordance with https://github.com/jonathanburrows/lavalav/issues/34

---
+++ Ontology/IAggregateRoot.cs
@@ -0,0 +1,14 @@
+﻿namespace lvl.Ontology
+{
+    /// <summary>
+    ///     Represents the main entity in a business model.
+    /// </summary>
+    /// <remarks>
+    ///     When making an object reference, an entity should only reference an aggregate root.
+    ///     
+    ///     If wanting to reference a non-aggregate root, use a foreign key id instead.
+    /// 
+    ///     If an entity references a non-aggregate root, and uses an object reference, then an exception will be thrown.
+    /// </remarks>
+    public interface IAggregateRoot { }
+}
---
+++ Ontology/IAggregateScope.cs
@@ -0,0 +1,13 @@
+﻿namespace lvl.Ontology
+{
+    /// <summary>
+    ///     Represents an entity which is part of a business process.
+    /// </summary>
+    /// <remarks>
+    ///     An entity should only be in 1 aggregate scope. If it should be in multiple, consider promoting it to an aggregate root.
+    /// </remarks>
+    /// <typeparam name="TAggregateRoot">
+    ///     A reference to the root entity, which represents which business process the aggregate scope belongs to.
+    /// </typeparam>
+    public interface IAggregateScope<TAggregateRoot> where TAggregateRoot : IAggregateRoot { }
+}
