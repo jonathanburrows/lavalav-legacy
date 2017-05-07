a8e0fd441227fe8cee7d38e140e29b2cc01a4eba
jonathanburrows <jonathanburrows@live.com>
2017-05-07 15:02:29 -0700

    added an abstract entity base class, in accordance with https://github.com/jonathanburrows/lavalav/issues/33

---
+++ Ontology.Tests/EntityTests.cs
@@ -0,0 +1,228 @@
+﻿using lvl.Ontology.Tests.Fixtures;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace lvl.Ontology.Tests
+{
+    [Collection(OntologyCollection.Name)]
+    public class EntityTests
+    {
+        [Fact]
+        public void It_will_be_abstract()
+        {
+            Assert.True(typeof(Entity).IsAbstract);
+        }
+
+        [Fact]
+        public void It_will_have_a_parameterless_constructor()
+        {
+            // GetConstructor(Types.EmptyType) wasnt used because the constructor is protected.
+            var constructors = typeof(Entity).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic);
+            var parameterlessConstructor = constructors.SingleOrDefault(c => !c.GetParameters().Any());
+
+            Assert.NotNull(parameterlessConstructor);
+        }
+
+        [Fact]
+        public void It_will_have_a_virtual_id_property()
+        {
+            var idPropertyInfo = typeof(Entity).GetProperty(nameof(Entity.Id));
+        }
+
+        [Fact]
+        public void Its_equal_will_be_false_when_obj_is_not_entity()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new NonEntity();
+
+            Assert.False(a.Equals(b));
+        }
+
+        [Fact]
+        public void Its_equal_will_be_false_when_obj_is_null()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+
+            Assert.False(a.Equals(null));
+        }
+
+        [Fact]
+        public void Its_equal_will_be_true_when_references_are_equal()
+        {
+            var a = new ComparisonEntity();
+            var b = a;
+
+            Assert.True(a.Equals(b));
+        }
+
+        [Fact]
+        public void Its_equal_will_be_false_when_of_different_type()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new DifferentTypeEntity { Id = 1 };
+
+            Assert.False(a.Equals(b));
+        }
+
+        [Fact]
+        public void Its_equal_will_be_false_when_its_id_is_zero()
+        {
+            var a = new ComparisonEntity { Id = 0 };
+            var b = new ComparisonEntity { Id = 1 };
+
+            Assert.False(a.Equals(b));
+        }
+
+        [Fact]
+        public void Its_equal_will_be_false_when_obj_id_is_zero()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new ComparisonEntity { Id = 0 };
+
+            Assert.False(a.Equals(b));
+        }
+
+        [Fact]
+        public void Its_equal_will_be_false_when_both_ids_are_zero()
+        {
+            var a = new ComparisonEntity { Id = 0 };
+            var b = new ComparisonEntity { Id = 0 };
+
+            Assert.False(a.Equals(b));
+        }
+
+        [Fact]
+        public void Its_equal_will_be_true_when_both_ids_are_matching()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new ComparisonEntity { Id = 1 };
+
+            Assert.True(a.Equals(b));
+        }
+
+        [Fact]
+        public void Its_equal_will_be_false_when_ids_are_different()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new ComparisonEntity { Id = 2 };
+
+            Assert.False(a.Equals(b));
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_true_if_both_are_null()
+        {
+            Entity a = null;
+            Entity b = null;
+
+            Assert.True(a == b);
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_false_if_a_is_null()
+        {
+            Entity a = null;
+            Entity b = new ComparisonEntity { Id = 1 };
+
+            Assert.False(a == b);
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_false_when_b_is_null()
+        {
+            Entity a = new ComparisonEntity { Id = 1 };
+            Entity b = null;
+
+            Assert.False(a == b);
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_true_when_references_are_equal()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = a;
+
+            Assert.True(a == b);
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_false_when_of_different_type()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new DifferentTypeEntity { Id = 1 };
+
+            Assert.False(a == b);
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_false_when_a_id_is_zero()
+        {
+            var a = new ComparisonEntity { Id = 0 };
+            var b = new ComparisonEntity { Id = 1 };
+
+            Assert.False(a == b);
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_false_when_b_id_is_zero()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new ComparisonEntity { Id = 0 };
+
+            Assert.False(a == b);
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_false_when_both_ids_are_zero()
+        {
+            var a = new ComparisonEntity { Id = 0 };
+            var b = new ComparisonEntity { Id = 0 };
+
+            Assert.False(a == b);
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_true_when_both_ids_are_matching()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new ComparisonEntity { Id = 1 };
+
+            Assert.True(a == b);
+        }
+
+        [Fact]
+        public void Its_equal_operator_will_be_false_when_ids_are_different()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new ComparisonEntity { Id = 2 };
+
+            Assert.False(a == b);
+        }
+
+        [Fact]
+        public void Its_GetHashCode_will_return_the_hash_of_entity_type_plus_id()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new ComparisonEntity { Id = 2 };
+
+            Assert.False(a == b);
+        }
+
+        [Fact]
+        public void Its_equals_works_with_linqs_contain()
+        {
+            var a = new ComparisonEntity { Id = 1 };
+            var b = new ComparisonEntity { Id = 1 };
+            var entities = new[] { a };
+
+            Assert.True(entities.Contains(b));
+        }
+
+        private class ComparisonEntity : Entity { }
+
+        private class DifferentTypeEntity : Entity { }
+
+        private class NonEntity { }
+    }
+}
---
+++ Ontology/Entity.cs
@@ -0,0 +1,101 @@
+﻿namespace lvl.Ontology
+{
+    /// <summary>
+    ///     Represents a model in a domain that can be persisted, and compared against other entities.
+    /// </summary>
+    public abstract class Entity
+    {
+        /// <summary>
+        ///     The primary key of the entity. Used in comparisons with other entities.
+        /// </summary>
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        ///     Compares two entities based on reference and identity.
+        /// </summary>
+        /// <param name="obj">The object to be compared against the entity.</param>
+        /// <returns>
+        ///     If the other object is not an entity, false.
+        ///     If the other object is null, then false.
+        ///     If the other object has the same reference, then true.
+        ///     If the other object is of a different type, then false.
+        ///     If either object has an id of zero, then false.
+        ///     If both objects have matching ids, then true.
+        ///     Otherwise, false.
+        /// </returns>
+        public override bool Equals(object obj)
+        {
+            var boxed = obj as Entity;
+
+            if (ReferenceEquals(boxed, null))
+            {
+                return false;
+            }
+            else if (ReferenceEquals(this, boxed))
+            {
+                return true;
+            }
+            else if (GetType() != boxed.GetType())
+            {
+                return false;
+            }
+            else if (Id == 0 || boxed.Id == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return Id == boxed.Id;
+            }
+        }
+
+        /// <summary>
+        ///     Will return the hash of the entity type concatonated with it's identifier.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            var uniqueKey = GetType().ToString() + Id;
+            return uniqueKey.GetHashCode();
+        }
+
+        /// <summary>
+        ///     Compares two entities based on reference and identity.
+        /// </summary>
+        /// <param name="a">First entity to be compared.</param>
+        /// <param name="b">Second entity to be compared.</param>
+        /// <returns>
+        ///     If both are null, then true.
+        ///     If only 1 is null, then false.
+        ///     If the two entities have the same reference, then true.
+        ///     If the two entities are of different type, then false.
+        ///     If either entity have an id of zero, then false.
+        ///     If entities have matching ids, then true.
+        ///     Otherwise, false.
+        /// </returns>
+        /// <remarks>Similar to Equals, but handles the case of the first arg being null.</remarks>
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+            {
+                return true;
+            }
+            else if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+            {
+                return false;
+            }
+            else
+            {
+                return a.Equals(b);
+            }
+        }
+
+        /// <summary>
+        ///     The complement of ==
+        /// </summary>
+        /// <param name="a">First entity to be compared.</param>
+        /// <param name="b">Second entity to be compared.</param>
+        public static bool operator !=(Entity a, Entity b) => !(a == b);
+
+        protected Entity() { }
+    }
+}
