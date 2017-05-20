7f5737ccb07701a218ca6c62c9cd8b6ec9d85ee9
jonathanburrows <jonathanburrows@live.com>
2017-05-19 19:30:25 -0700

    adding extension methods, in accordance with https://github.com/jonathanburrows/lavalav/issues/55

---
+++ Oidc.AuthorizationServer.Tests/ServiceCollectionExtensionTests.cs
@@ -0,0 +1,96 @@
+﻿using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using IdentityServer4.Validation;
+using lvl.Oidc.AuthorizationServer.Seeder;
+using lvl.Oidc.AuthorizationServer.Services;
+using lvl.Oidc.AuthorizationServer.Stores;
+using lvl.Oidc.AuthorizationServer.Tests.Fixtures;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Xunit;
+
+namespace lvl.Oidc.AuthorizationServer.Tests
+{
+    [Collection(nameof(OidcAuthorizationServerCollection))]
+    public class ServiceCollectionExtensionTests
+    {
+        private IServiceProvider ServiceProvider { get; }
+
+        public ServiceCollectionExtensionTests(ServiceProviderFixture serviceProviderFixture)
+        {
+            ServiceProvider = serviceProviderFixture.ServiceProvider;
+        }
+
+        [Fact]
+        public void It_will_allow_cors_policy_service_to_be_resolved()
+        {
+            var corsPolicyService = ServiceProvider.GetRequiredService<ICorsPolicyService>();
+
+            Assert.NotNull(corsPolicyService);
+        }
+
+        [Fact]
+        public void It_will_allow_resource_owner_password_validator_to_be_resolved()
+        {
+            var resourceOwnerPasswordValidator = ServiceProvider.GetRequiredService<IResourceOwnerPasswordValidator>();
+
+            Assert.NotNull(resourceOwnerPasswordValidator);
+        }
+
+        [Fact]
+        public void It_will_allow_client_store_to_be_resolved()
+        {
+            var clientStore = ServiceProvider.GetRequiredService<IClientStore>();
+
+            Assert.NotNull(clientStore);
+        }
+
+        [Fact]
+        public void It_will_allow_resource_store_to_be_resolved()
+        {
+            var resourceStore = ServiceProvider.GetRequiredService<IResourceStore>();
+
+            Assert.NotNull(resourceStore);
+        }
+
+        [Fact]
+        public void It_will_allow_password_hasher_to_be_resolved()
+        {
+            var passwordHasher = ServiceProvider.GetRequiredService<PasswordHasher>();
+
+            Assert.NotNull(passwordHasher);
+        }
+
+        [Fact]
+        public void It_will_allow_seeder_to_be_resolved()
+        {
+            var seeder = ServiceProvider.GetRequiredService<OidcAuthorizationServerSeeder>();
+
+            Assert.NotNull(seeder);
+        }
+
+        [Fact]
+        public void It_will_allow_user_store_to_be_resolved()
+        {
+            var userStore = ServiceProvider.GetRequiredService<UserStore>();
+
+            Assert.NotNull(userStore);
+        }
+
+        [Fact]
+        public void It_will_allow_persisted_grant_store_to_be_resolved()
+        {
+            var persistedGrantStore = ServiceProvider.GetRequiredService<IPersistedGrantStore>();
+
+            Assert.NotNull(persistedGrantStore);
+        }
+
+        [Fact]
+        public void It_will_allow_oidc_authorization_server_options_to_be_resolved()
+        {
+            var options = ServiceProvider.GetRequiredService<OidcAuthorizationServerOptions>();
+
+            Assert.NotNull(options);
+        }
+    }
+}
---
+++ Oidc.AuthorizationServer/OidcAuthorizationServerApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// ReSharper disable once CheckNamespace In compliance with Microsoft's conventions.
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    ///     Sets up an Mvc application to act as an open id authentication server.
+    /// </summary>
+    public static class OidcAuthorizationServerApplicationBuilderExtensions
+    {
+        /// <summary>
+        ///     Sets up an Mvc application to act as an open id authentication server.
+        /// </summary>
+        /// <param name="applicationBuilder">The application being configured.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="applicationBuilder"/> is null.</exception>
+        /// <returns>The given application.</returns>
+        public static IApplicationBuilder UseOidcAuthorizationServer(this IApplicationBuilder applicationBuilder)
+        {
+            if (applicationBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(applicationBuilder));
+            }
+
+            return applicationBuilder
+                .UseCookieAuthentication()
+                .UseIdentityServer();
+        }
+    }
+}
